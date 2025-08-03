36d10d021e332c493f058ef50dee0c930f50a649
Joao Cantuario <pedro.rissardi@gmail.com>
2025-08-03 00:45:52 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/zezin/workshopmongo/config/Instantiation.java
+++ src/main/java/com/zezin/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.zezin.workshopmongo.config;
 
 import com.zezin.workshopmongo.domain.Post;
 import com.zezin.workshopmongo.domain.User;
+import com.zezin.workshopmongo.dto.AuthorDTO;
 import com.zezin.workshopmongo.repository.PostRepository;
 import com.zezin.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +38,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
+        //salvar primeiro os usuarios para a persistencia de dados
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
