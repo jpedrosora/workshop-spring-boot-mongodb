ca0dafe188187784f403765c4979c58de4a6a568
Joao Cantuario <pedro.rissardi@gmail.com>
2025-08-05 23:34:14 -0300

    Consulta simples com @Query

--- src/main/java/com/zezin/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/zezin/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.zezin.workshopmongo.repository;
 
 import com.zezin.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,12 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    //metodo alternativo
+    //https://www.mongodb.com/pt-br/docs/manual/reference/operator/query/regex/
+    //especificando uma consulta explicitamente com um json, ?0 para dizer que é o primeiro parametro recebido
+    @Query("{'title' : {$regex: ?0, $options :  'i' } }")
+    List<Post> searchTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
